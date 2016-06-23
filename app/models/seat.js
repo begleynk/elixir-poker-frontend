38ed837f69b4ab05d943119bcd790614662ddc91
Niklas Begley <niklas.begley@gmail.com>
2016-06-23 11:05:08 +0100

    Starting to setup routes

---
+++ app/models/seat.js
@@ -0,0 +1,8 @@
+import Model from 'ember-data/model';
+import DS from 'ember-data';
+// import { belongsTo, hasMany } from 'ember-data/relationships';
+
+export default Model.extend({
+  status: DS.attr(),
+  position: DS.attr(),
+});
--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('tables');
+  this.route('table', { path: '/tables/:table_id' });
+  this.route('sign-up');
 });
 
 export default Router;
